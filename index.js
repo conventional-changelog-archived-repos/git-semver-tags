bacf7358b1cc183fc3c19430e887d71616949c58
Mario Nebl <hello@herebecode.com>
2016-06-03 01:54:13 +0200

    refactor: use git-fs-repo instead of git cli
    *  introduces [git-fs-repo](https://github.com/chrisdickinson/git-fs-repo)
    *  removes reliance on `git cli`
    *  replaces regex parsing of `git log` with iteration over [^/refs/tags/*]
    *  retains sorting of results of previous versions
    
    I had some issues with the current RegExp technique skipping some git
    tags in one of my projects. I am still investigating this and a second,
    short-term mergeable PR will land tomorrow. While dabbling around with
    js-only git implementations the current PR came into being. This is
    meant as basis for discussion about a move to a js-only
    conventional-changelog.
    
    fixes #1
    depends on chrisdickinson/git-load-refs#3

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-'use strict';
-var exec = require('child_process').exec;
-var semverValid = require('semver').valid;
-var regex = /tag:\s*(.+?)[,\)]/gi;
-var cmd = 'git log --decorate --no-color';
+var semver = require('semver');
 
-module.exports = function(callback) {
-  exec(cmd, {
-    maxBuffer: Infinity
-  }, function(err, data) {
-    if (err) {
-      callback(err);
-      return;
+var findIndexByHash = require('./lib/find-index-by-hash');
+var getCommits = require('./lib/get-commits');
+var getSemverTags = require('./lib/get-semver-tags');
+var loadRepository = require('./lib/load-repository');
+
+module.exports = function gitSemverTags(callback) {
+  loadRepository(function(error, repository) {
+    if (error) {
+      return callback(error);
     }
 
-    var tags = [];
-
-    data.split('\n').forEach(function(decorations) {
-      var match;
-      while (match = regex.exec(decorations)) {
-        var tag = match[1];
-        if (semverValid(tag)) {
-          tags.push(tag);
-        }
+    // Get a list of commits
+    getCommits(repository, function(error, commits) {
+      if (error) {
+        return callback(error);
       }
-    });
 
-    callback(null, tags);
+      var tagNames = getSemverTags(repository)
+        .sort(function(aTag, bTag) {
+          if (aTag.hash === bTag.hash) {
+            return semver.compare(bTag.name, aTag.name);
+          }
+
+          var aCommit = findIndexByHash(commits, aTag.hash);
+          var bCommit = findIndexByHash(commits, bTag.hash);
+          return bCommit - aCommit;
+        })
+        // map out the name
+        .map(function(tag) {
+          return tag.name;
+        });
+
+      return callback(null, tagNames);
+    });
   });
 };
