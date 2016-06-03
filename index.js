813cbe456448d6933766630902e2f1b859f6c39c
Mario Nebl <hello@herebecode.com>
2016-06-03 09:24:56 +0200

    docs: jsdoc all the things

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ var getCommits = require('./lib/get-commits');
 var getSemverTags = require('./lib/get-semver-tags');
 var loadRepository = require('./lib/load-repository');
 
-module.exports = function gitSemverTags(callback) {
+/**
+ * Get semantic version git tags of repository at process.cwd()
+ *
+ * @param  {MainCallback} callback function to execute after information retrieval
+ */
+function gitSemverTags(callback) {
+  // Initialize repository
   loadRepository(function(error, repository) {
     if (error) {
       return callback(error);
@@ -17,12 +23,15 @@ module.exports = function gitSemverTags(callback) {
         return callback(error);
       }
 
+      // Get a list of tags matching semver pattern
       var tagNames = getSemverTags(repository)
         .sort(function(aTag, bTag) {
+          // if tags reference same hash sort descending by semantic version
           if (aTag.hash === bTag.hash) {
             return semver.compare(bTag.name, aTag.name);
           }
 
+          // sort tags descending by occurence in commits list
           var aCommit = findIndexByHash(commits, aTag.hash);
           var bCommit = findIndexByHash(commits, bTag.hash);
           return bCommit - aCommit;
@@ -36,3 +45,13 @@ module.exports = function gitSemverTags(callback) {
     });
   });
 };
+
+module.exports = gitSemverTags;
+
+/**
+ * Main callback executed after all information has been collected
+ *
+ * @typedef {function} MainCallback
+ * @param {(Error|null)} error - encountered error, if any
+ * @param {array} [tags] - semantic git tags of repository at process.cwd()
+ */
